3d73999f718698b00515e644c9c9cb3da8046f97
antomfdez <antonio.mesa.fdez@gmail.com>
2024-09-26 13:46:43 +0200

    Updated

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,12 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.window_padding = {
+  left = 5,
+  right = 0,
+  top = 5,
+  bottom = 0,
+}
 config.font = wezterm.font("Cascadia Mono NF")
 config.font_size = 14
 
