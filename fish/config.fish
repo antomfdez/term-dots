12d75c950cc864c17e1432dc2fa3f01e1d76f7ff
antomfdez <antonio.mesa.fdez@gmail.com>
2024-09-26 18:28:56 +0200

    Updated fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	alias hx "flatpak run com.helix_editor.Helix"
 end
 source "/var/home/antomfdez/emsdk/emsdk_env.fish"
