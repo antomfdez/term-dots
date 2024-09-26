8f5ac795aff8634b2118a224193b19e91d2878c4
antomfdez <antonio.mesa.fdez@gmail.com>
2024-09-26 18:28:23 +0200

    Updated

--- wezterm.lua
+++ wezterm.lua
@@ -7,13 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.window_padding = {
-  left = 5,
-  right = 0,
-  top = 5,
+  left = 10,
+  right = 10,
+  top = 10,
   bottom = 0,
 }
 config.font = wezterm.font("Cascadia Mono NF")
-config.font_size = 14
+config.font_size = 13
 
 config.enable_tab_bar = false
 
@@ -22,7 +22,7 @@ config.window_decorations = "RESIZE"
 config.color_scheme = 'rose-pine'
 
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.window_background_image = "/var/home/antomfdez/Pictures/fsoc.jpg"
 
 -- and finally, return the configuration to wezterm
 return config
